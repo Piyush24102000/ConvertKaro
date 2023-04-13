6aa8fa0e16d6aa40c25cf4249cda951b16b1c764
PiyushTale <64774043+Piyush24102000@users.noreply.github.com>
2023-04-13 17:11:22 +0530

    Update imageMin.js

--- src/pages/api/imageMin.js
+++ src/pages/api/imageMin.js
@@ -1,69 +1,69 @@
-import multer from 'multer';
-const util = require('util');
-import imagemin from 'imagemin';
-import imageminMozjpeg from 'imagemin-mozjpeg';
-const upload = multer();
+// import multer from 'multer';
+// const util = require('util');
+// import imagemin from 'imagemin';
+// import imageminMozjpeg from 'imagemin-mozjpeg';
+// const upload = multer();
 
-export const config = {
-    api: {
-        bodyParser: false,
-        responseLimit: false,
+// export const config = {
+//     api: {
+//         bodyParser: false,
+//         responseLimit: false,
 
-    },
-};
+//     },
+// };
 
-export default async function handler(req, res) {
-    switch (req.method) {
-        case 'POST':
-            await bufferImage(req, res)
-            break;
-    }
-}
+// export default async function handler(req, res) {
+//     switch (req.method) {
+//         case 'POST':
+//             await bufferImage(req, res)
+//             break;
+//     }
+// }
 
-const bufferImage = async (req, res) => {
-    try {
-        const multerMiddleware = util.promisify(upload.single('image'));
-        await multerMiddleware(req, res);
-        const fileBuffer = req.file.buffer;
+// const bufferImage = async (req, res) => {
+//     try {
+//         const multerMiddleware = util.promisify(upload.single('image'));
+//         await multerMiddleware(req, res);
+//         const fileBuffer = req.file.buffer;
 
-        // Get the desired file size from the request parameters
-        const desiredSize = parseInt(req.query.size) || 1000
-        // const desiredSize = 400
+//         // Get the desired file size from the request parameters
+//         const desiredSize = parseInt(req.query.size) || 1000
+//         // const desiredSize = 400
 
-        const maxIterations = 8; // set a maximum number of iterations
-        let quality = 75;
-        let compressedBuffer = null;
-        let iterations = 0;
+//         const maxIterations = 8; // set a maximum number of iterations
+//         let quality = 75;
+//         let compressedBuffer = null;
+//         let iterations = 0;
 
-        while (compressedBuffer === null || compressedBuffer.length / 1000 > desiredSize) {
-            // check if the maximum number of iterations has been reached
-            if (iterations > maxIterations) {
-                return res.status(400).send({msg:'Unable to compress image to desired size. Please increase/decrease size'});
-            }
+//         while (compressedBuffer === null || compressedBuffer.length / 1000 > desiredSize) {
+//             // check if the maximum number of iterations has been reached
+//             if (iterations > maxIterations) {
+//                 return res.status(400).send({msg:'Unable to compress image to desired size. Please increase/decrease size'});
+//             }
 
-            compressedBuffer = await imagemin.buffer(fileBuffer, {
-                plugins: [
-                    imageminMozjpeg({ quality }),
-                ],
-            });
+//             compressedBuffer = await imagemin.buffer(fileBuffer, {
+//                 plugins: [
+//                     imageminMozjpeg({ quality }),
+//                 ],
+//             });
 
-            quality = Math.round(quality * (desiredSize / (compressedBuffer.length / 1000)));
-            iterations++;
-        }
+//             quality = Math.round(quality * (desiredSize / (compressedBuffer.length / 1000)));
+//             iterations++;
+//         }
 
-        // Convert the compressed image buffer to a base64 data URL
-        const base64Image = compressedBuffer.toString('base64');
-        const dataUrl = `data:image/jpeg;base64,${base64Image}`;
+//         // Convert the compressed image buffer to a base64 data URL
+//         const base64Image = compressedBuffer.toString('base64');
+//         const dataUrl = `data:image/jpeg;base64,${base64Image}`;
 
-        // Send the data URL as a response
-        const responseObj = {
-            dataUrl,
-        };
-        return res.status(200).json(responseObj);
+//         // Send the data URL as a response
+//         const responseObj = {
+//             dataUrl,
+//         };
+//         return res.status(200).json(responseObj);
 
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).send("An error occurred while uploading the file.");
+//     } catch (error) {
+//         console.log(error.message)
+//         res.status(500).send("An error occurred while uploading the file.");
 
-    }
-}
+//     }
+// }
